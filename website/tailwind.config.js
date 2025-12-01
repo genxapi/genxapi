ae4d2cde9a3b2ca3e6c40793884f3d44e6cb8efc
Eduardo <EduardoAC@users.noreply.github.com>
2025-12-01 06:26:15 +0000

    Allow using tailwind css within the MD files

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/content/**/*.{md,mdx}"
+    "./src/**/*.{js,ts,jsx,tsx,md,mdx}"
   ],
   theme: {
     extend: {
